143866553f7b899bb7c927bc92c22dccddae09b1
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-04-15 09:53:25 +0500

    connecting db

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/";
+// const URI = "mongodb://127.0.0.1:27017/";
+
+const URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
     try {
         await mongoose.connect(URI);
         console.log("Connection Successful to DB");
     } catch (error) {
-        console.error("DB connection failed");
+        console.error(error);
         process.exit(0);
     }
 };
